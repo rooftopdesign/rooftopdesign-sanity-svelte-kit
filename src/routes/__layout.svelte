7f02513d4e504cc8accae89b101e40e481c17738
rooftopdesign <info@rooftop.de>
2022-10-29 10:05:03 +0200

    modified:   src/routes/__layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,7 @@
   import Footer from '$lib/Footer.svelte'
 </script>
 
-<Header blogTitle={'SvelteKit x Sanity Blog'} />
+<Header blogTitle={'Rooftop Design Blog'} />
 
 <main style="padding: 1em">
   <slot />
